fa72ed97f91ec3a123cf3cf39f0f554214eb266e
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 08:37:22 -0300

    Update cerebro.py

--- iara/cerebro.py
+++ iara/cerebro.py
@@ -1,12 +1,16 @@
 from iara.estado import mudar_estado
 from iara.conversa import falar
 from iara.ciclo import observar
+from iara.eventos import adicionar_evento
 
 def iniciar():
+
     mudar_estado("iniciando")
 
     falar()
 
     mudar_estado("observando")
+
+    adicionar_evento("sistema_iniciado")
 
     observar()
