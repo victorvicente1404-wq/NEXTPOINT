471ff7a71339863b9dad1f8825d3b905f5a6eb07
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 11:57:09 -0300

    Add new fields to CONTEXTO dictionary

--- iara/contexto.py
+++ iara/contexto.py
@@ -1,6 +1,9 @@
 CONTEXTO = {
     "usuario": None,
+    "apelido": None,
     "assunto": None,
     "ultima_pergunta": None,
-    "ultima_resposta": None
+    "ultima_resposta": None,
+    "emocao_detectada": None,
+    "conversando": False
 }
