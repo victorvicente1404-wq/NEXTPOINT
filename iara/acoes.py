9ea67d45a03821c59cf59ee9ce779226387083d3
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 09:31:31 -0300

    Create acoes.py

---
+++ iara/acoes.py
@@ -0,0 +1,16 @@
+def executar(acao):
+
+    if acao == "observar":
+        print("👀 Observando o ambiente...")
+
+    elif acao == "cumprimentar":
+        print("😊 Olá!")
+
+    elif acao == "conversar":
+        print("💬 Vamos conversar!")
+
+    elif acao == "responder":
+        print("🤔 Pensando na resposta...")
+
+    else:
+        print("...")
