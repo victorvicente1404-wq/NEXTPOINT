e4cc50fc3d242ec3e0e2579616d734620f2bf9a0
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 08:09:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from iara.conversa import falar
 from config import NOME_PROJETO, VERSAO_PROJETO
 
-print("Iniciando NextPoint...")
-falar()
+print(f"Iniciando {NOME_PROJETO}...")
+print(f"Versão {VERSAO_PROJETO}")
