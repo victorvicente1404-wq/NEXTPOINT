c7de819aaf0e8c06b2e357e60655757e809af3a1
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 07:56:00 -0300

    Update conversa.py

--- iara/conversa.py
+++ iara/conversa.py
@@ -2,7 +2,7 @@ from iara.memoria import lembrar_nome
 from iara.personalidade import NOME
 
 def falar():
-    print("Olá! Eu sou a Iara.")
+    print(f"Olá! Eu sou a {NOME}.")
 
     nome = lembrar_nome()
 
