cb45807e0d814988fbf1f9690b68cb92217f0cc3
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 10:07:20 -0300

    Update chat.py

--- iara/chat.py
+++ iara/chat.py
@@ -1,6 +1,10 @@
 from iara.motor_conversa import responder
+from iara.historico import salvar
+from iara.estado import mudar_estado
 
 def conversar():
+
+    mudar_estado("conversando")
 
     while True:
 
@@ -8,8 +12,11 @@ def conversar():
 
         if mensagem.lower() == "sair":
             print("Iara: Até logo! 😊")
+            mudar_estado("ociosa")
             break
 
         resposta = responder(mensagem)
+
+        salvar(mensagem, resposta)
 
         print(f"Iara: {resposta}")
