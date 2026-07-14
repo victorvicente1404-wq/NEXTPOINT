d0eaba4551b6190e68ce6710e09732750bbd9011
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 08:39:47 -0300

    Create respostas.py

---
+++ iara/respostas.py
@@ -0,0 +1,17 @@
+from iara.humor import obter_humor
+
+def responder_boas_vindas(nome):
+
+    humor = obter_humor()
+
+    if humor == "feliz":
+        print(f"😊 Oi, {nome}! Que bom te ver!")
+
+    elif humor == "animada":
+        print(f"😄 E aí, {nome}! Seja muito bem-vindo!")
+
+    elif humor == "curiosa":
+        print(f"🤔 Olá, {nome}. O que vamos fazer hoje?")
+
+    else:
+        print(f"Olá, {nome}.")
