837c7bc31bf8e5c7445d59085732152616003ebb
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 20:29:21 -0300

    Update contexto.py

--- iara/contexto.py
+++ iara/contexto.py
@@ -1,21 +1,31 @@
 """
-Contexto temporário da conversa.
-
-Tudo aqui é perdido quando o programa fecha.
+Contexto da conversa.
 """
 
 CONTEXTO = {
+
     "ultima_mensagem": None,
-    "ultima_resposta": None,
+
     "ultima_intencao": None,
-    "assunto": None
+
+    "ultima_resposta": None
 }
 
 
-def atualizar(mensagem, intencao, resposta):
+def atualizar(
+
+    mensagem,
+
+    intencao,
+
+    resposta
+
+):
 
     CONTEXTO["ultima_mensagem"] = mensagem
+
     CONTEXTO["ultima_intencao"] = intencao
+
     CONTEXTO["ultima_resposta"] = resposta
 
 
