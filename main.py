1c0595dbccb6245c4d3ab0bba3ef3c911297110d
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 09:52:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from config import NOME_PROJETO, VERSAO_PROJETO
 from iara.cerebro import iniciar
+from iara.chat import conversar
 
 print(f"Iniciando {NOME_PROJETO}...")
 print(f"Versão {VERSAO_PROJETO}")
 
 iniciar()
+
+conversar()
