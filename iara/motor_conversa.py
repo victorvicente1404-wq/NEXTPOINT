5f268bbefadfb36a80416519784ccbd237c0d496
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 20:59:40 -0300

    Update motor_conversa.py

--- iara/motor_conversa.py
+++ iara/motor_conversa.py
@@ -1,48 +1,40 @@
+"""
+Motor principal da conversa.
+"""
+
 from .interpretador import interpretar
-
-from .contexto import (
-
-    atualizar,
-
-    obter
-
-)
-
-from .memoria import carregar
-
+from .contexto import atualizar, obter
+from .memoria import carregar, aprender
 from .raciocinio import decidir
-
 from .respostas import responder
+from .acoes import executar
 
 
 def conversar(mensagem):
 
     dados = interpretar(mensagem)
 
+    if dados["entidades"]:
+        aprender(dados["entidades"])
+
     contexto = obter()
 
     memoria = carregar()
 
     decisao = decidir(
-
         dados,
-
         contexto,
-
         memoria
-
     )
+
+    executar(decisao)
 
     resposta = responder(decisao)
 
     atualizar(
-
         mensagem,
-
         dados["intencao"],
-
         resposta
-
     )
 
     return resposta
