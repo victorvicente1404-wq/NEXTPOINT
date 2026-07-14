c4a5eb62a4e7c4de9a9972a7bd396847569178ea
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 08:16:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from iara.conversa import falar
 from config import NOME_PROJETO, VERSAO_PROJETO
 from iara.estado import mudar_estado, estado_atual
+from iara.conversa import falar
 
 print(f"Iniciando {NOME_PROJETO}...")
 print(f"Versão {VERSAO_PROJETO}")
+
+mudar_estado("conversando")
+
+falar()
+
+mudar_estado("ociosa")
+
+print(f"Estado atual: {estado_atual()}")
