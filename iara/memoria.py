4cc971f9550b004973ba7c444d8007237e64fb88
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 20:29:37 -0300

    Update memoria.py

--- iara/memoria.py
+++ iara/memoria.py
@@ -1,43 +1,59 @@
 """
-Memória permanente.
-
-Tudo que estiver aqui continua existindo
-mesmo depois que a Iara for fechada.
+Memória permanente da Iara.
 """
 
 import json
+
 from pathlib import Path
 
 
 CAMINHO = (
+
     Path(__file__).parent.parent
+
     / "data"
+
     / "memoria.json"
+
 )
 
 
 def carregar():
 
     with open(
+
         CAMINHO,
+
         "r",
+
         encoding="utf-8"
+
     ) as arquivo:
 
         return json.load(arquivo)
 
 
+
 def salvar(memoria):
 
     with open(
+
         CAMINHO,
+
         "w",
+
         encoding="utf-8"
+
     ) as arquivo:
 
         json.dump(
+
             memoria,
+
             arquivo,
+
             indent=4,
+
             ensure_ascii=False
+
         )
