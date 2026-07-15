d1e1c3468deb1f6aefe866d9e3de1d8ae9f73d3b
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 08:05:06 -0300

    Implement Objetivo class with state management

--- iara/modelos/objetivo.py
+++ iara/modelos/objetivo.py
@@ -1,1 +1,36 @@
+"""
+Modelo de Objetivo.
+"""
 
+
+class Objetivo:
+
+    def __init__(self):
+
+        self.tipo = ""
+
+        self.parametros = {}
+
+        self.plano = []
+
+        self.resultado = None
+
+        self.estado = "novo"
+
+        self.erro = None
+
+    def iniciar(self):
+
+        self.estado = "executando"
+
+    def concluir(self, resultado=None):
+
+        self.estado = "concluido"
+
+        self.resultado = resultado
+
+    def falhar(self, erro):
+
+        self.estado = "erro"
+
+        self.erro = erro
