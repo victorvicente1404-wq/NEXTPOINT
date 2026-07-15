d6e3171e4c04f01623af1769de541d533f7b8900
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 09:48:39 -0300

    Add capabilities definitions in capacidades.py

---
+++ iara/agentes/capacidades.py
@@ -0,0 +1,21 @@
+"""
+Capacidades conhecidas pela Iara.
+"""
+
+ABRIR_PROGRAMA = "abrir_programa"
+
+FECHAR_PROGRAMA = "fechar_programa"
+
+ABRIR_ARQUIVO = "abrir_arquivo"
+
+LER_ARQUIVO = "ler_arquivo"
+
+SALVAR_ARQUIVO = "salvar_arquivo"
+
+EXECUTAR_COMANDO = "executar_comando"
+
+CONSULTAR_MEMORIA = "consultar_memoria"
+
+APRENDER = "aprender"
+
+PESQUISAR_INTERNET = "pesquisar_internet"
