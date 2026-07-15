ca144cc4e1572d1c69c510b903172448027c87fb
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 10:01:37 -0300

    Update gerenciador.py

--- iara/recursos/gerenciador.py
+++ iara/recursos/gerenciador.py
@@ -1,1 +1,27 @@
+class GerenciadorRecursos:
 
+    def __init__(self):
+
+        self.agentes = {}
+
+        self.ferramentas = {}
+
+        self.modelos = {}
+
+        self.plugins = {}
+
+    def registrar_agente(self, agente):
+
+        self.agentes[agente.nome] = agente
+
+    def registrar_ferramenta(self, ferramenta):
+
+        self.ferramentas[ferramenta.nome] = ferramenta
+
+    def registrar_modelo(self, modelo):
+
+        self.modelos[modelo.nome] = modelo
+
+    def registrar_plugin(self, plugin):
+
+        self.plugins[plugin.nome] = plugin
