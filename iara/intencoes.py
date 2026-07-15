c448c4d5e2468846d4331a65b84984eca48ea8e0
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-14 21:08:19 -0300

    Create intencoes.py

---
+++ iara/intencoes.py
@@ -0,0 +1,45 @@
+"""
+Banco de intenções da Iara.
+
+Cada intenção possui diversas formas
+de ser escrita pelo usuário.
+"""
+
+INTENCOES = {
+
+    "cumprimento": [
+
+        "oi",
+        "ola",
+        "eae",
+        "fala",
+        "opa",
+        "bom dia",
+        "boa tarde",
+        "boa noite"
+
+    ],
+
+    "despedida": [
+
+        "tchau",
+        "falou",
+        "ate logo",
+        "ate mais"
+
+    ],
+
+    "consultar_nome": [
+
+        "qual meu nome",
+        "qual o meu nome",
+        "qual e meu nome",
+        "qual e o meu nome",
+        "como eu me chamo",
+        "quem sou eu",
+        "voce sabe meu nome",
+        "lembra meu nome"
+
+    ]
+
+}
