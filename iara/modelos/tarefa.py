5960f8e8640e3f336450f7c4e48ee06610c11a2a
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 09:45:32 -0300

    Update tarefa.py

--- iara/modelos/tarefa.py
+++ iara/modelos/tarefa.py
@@ -1,1 +1,24 @@
+"""
+Modelo de Tarefa.
+"""
 
+
+class Tarefa:
+
+    def __init__(self):
+
+        self.id = None
+
+        self.nome = ""
+
+        self.estado = "nova"
+
+        self.progresso = 0
+
+        self.resultado = None
+
+        self.erro = None
+
+        self.passos = []
+
+        self.atual = 0
