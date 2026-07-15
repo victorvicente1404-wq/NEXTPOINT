76bd055f8183042fe2e61bd7c3f00de0bbcdae9e
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 15:39:34 -0300

    Add EstadoIA class for internal state management

--- iara/brain/estado.py
+++ iara/brain/estado.py
@@ -1,1 +1,20 @@
+"""
+Estado interno da Iara.
+"""
 
+
+class EstadoIA:
+
+    def __init__(self):
+
+        self.ocupada = False
+
+        self.pensando = False
+
+        self.executando = False
+
+        self.ultima_resposta = ""
+
+        self.humor = "neutro"
+
+        self.confianca = 1.0
