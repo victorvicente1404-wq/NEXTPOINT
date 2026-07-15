eee08b8c4583ddc0519a398c3dfea9e3522ca952
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 10:05:51 -0300

    Update evento.py

--- iara/eventos/evento.py
+++ iara/eventos/evento.py
@@ -1,1 +1,17 @@
+class EventoSistema:
 
+    def __init__(self):
+
+        self.id = ""
+
+        self.tipo = ""
+
+        self.origem = ""
+
+        self.destino = ""
+
+        self.dados = {}
+
+        self.prioridade = 0
+
+        self.cancelado = False
