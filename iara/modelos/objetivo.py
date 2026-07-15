4846e3795cd19b142fd003f674f909ff06417c05
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 08:09:36 -0300

    Refactor resultado and erro handling in objetivo.py
    Refactor methods to handle result and error states.

--- iara/modelos/objetivo.py
+++ iara/modelos/objetivo.py
@@ -13,24 +13,8 @@ class Objetivo:
 
         self.plano = []
 
-        self.resultado = None
-
         self.estado = "novo"
 
+        self.resultado = None
+
         self.erro = None
-
-    def iniciar(self):
-
-        self.estado = "executando"
-
-    def concluir(self, resultado=None):
-
-        self.estado = "concluido"
-
-        self.resultado = resultado
-
-    def falhar(self, erro):
-
-        self.estado = "erro"
-
-        self.erro = erro
