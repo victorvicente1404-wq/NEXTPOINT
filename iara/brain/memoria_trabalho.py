3b826221f1db0f10ebd1acd6fdf8b0f852442c6e
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 15:40:04 -0300

    Implement MemoriaTrabalho class for conversation history

--- iara/brain/memoria_trabalho.py
+++ iara/brain/memoria_trabalho.py
@@ -1,1 +1,28 @@
+"""
+Memória temporária da conversa.
+"""
 
+
+class MemoriaTrabalho:
+
+    def __init__(self):
+
+        self.historico = []
+
+    def adicionar(self, usuario, iara):
+
+        self.historico.append({
+
+            "usuario": usuario,
+
+            "iara": iara
+
+        })
+
+        if len(self.historico) > 20:
+
+            self.historico.pop(0)
+
+    def obter(self):
+
+        return self.historico
