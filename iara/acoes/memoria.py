3bcc3be32f98164a8c6a4b4e2ea4483bbd17600c
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 09:31:33 -0300

    Update memoria.py

--- iara/acoes/memoria.py
+++ iara/acoes/memoria.py
@@ -1,1 +1,28 @@
+"""
+Ações relacionadas à memória.
+"""
 
+from .base import registrar
+
+from ..pipeline.memoria import (
+    aprender,
+    consultar
+)
+
+
+@registrar("ler_memoria")
+def ler_memoria(evento, passo):
+
+    chave = passo["chave"]
+
+    evento.objetivo.resultado = consultar(chave)
+
+
+@registrar("salvar_memoria")
+def salvar_memoria(evento, passo):
+
+    aprender(
+
+        evento.objetivo.parametros["dados"]
+
+    )
