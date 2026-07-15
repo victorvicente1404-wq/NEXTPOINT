8494d54aedf9cd6e4e93d5fb7b24a2a8500760b6
victorvicente1404-wq <victorvicente1404@gmail.com>
2026-07-15 09:39:07 -0300

    Update base.py

--- iara/ferramentas/base.py
+++ iara/ferramentas/base.py
@@ -1,1 +1,39 @@
+"""
+Sistema base de ferramentas da Iara.
+"""
 
+
+FERRAMENTAS = []
+
+
+class Ferramenta:
+
+    nome = "Ferramenta"
+
+    descricao = ""
+
+    def executar(self, parametros):
+
+        raise NotImplementedError
+
+
+
+def registrar(classe):
+
+    FERRAMENTAS.append(
+        classe()
+    )
+
+    return classe
+
+
+
+def encontrar(nome):
+
+    for ferramenta in FERRAMENTAS:
+
+        if ferramenta.nome == nome:
+
+            return ferramenta
+
+    return None
